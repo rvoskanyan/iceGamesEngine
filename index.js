409966f0828b1c6bd508b5e7b7522fdd68a692ed
rvoskanyan <rvoskanyan@mmtr.ru>
2021-07-24 11:18:53 +0300

    first commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.set('views', 'views');
 app.use('/admin', adminRoutes);
 app.use('/', clientRoutes);
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 4000;
 
 const start = async () => {
   try {
