7f37a87fc3638dd7ab5af8158992d0de0a1e2de2
rvoskanyan <rvoskanyan@mmtr.ru>
2021-07-27 17:35:42 +0300

    first commit

--- controllers/client/index.js
+++ controllers/client/index.js
@@ -1,5 +1,11 @@
 const index = async (req, res) => {
-  res.json("Index client");
+  res.json({
+    PORT: process.env.PORT,
+    IGS_DB_USERNAME: process.env.IGS_DB_USERNAME,
+    IGS_DB_PASSWORD: process.env.IGS_DB_PASSWORD,
+    IGS_DB_NAME: process.env.IGS_DB_NAME,
+    NODE_ENV: process.env.NODE_ENV,
+  });
 }
 
 module.exports = {
