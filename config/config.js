9f95418dac37e2c82a9f4fbbca631e24a24d03d2
rvoskanyan <rvoskanyan@mmtr.ru>
2021-09-23 13:16:13 +0300

    change minimizer

--- config/config.js
+++ config/config.js
@@ -12,9 +12,6 @@ module.exports = {
     database: process.env.IGS_DB_NAME,
     host: '127.0.0.1',
     dialect: 'mysql',
-    dialectOptions: {
-      socketPath: '/tmp/mysql.sock',
-    },
   },
   production: {
     username: process.env.IGS_DB_USERNAME,
@@ -22,8 +19,5 @@ module.exports = {
     database: process.env.IGS_DB_NAME,
     host: '127.0.0.1',
     dialect: 'mysql',
-    dialectOptions: {
-      socketPath: '/tmp/mysql.sock',
-    },
   }
 };
