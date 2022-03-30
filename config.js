4333e9f884226b0c2bf2f288f17f2e28738149f8
rvoskanyan <rvoskanyan@mmtr.ru>
2022-03-30 14:47:56 +0300

    client

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //websiteAddress: 'http://141.8.194.196:4000/',
-  websiteAddress: 'http://localhost:4000/',
+  websiteAddress: 'http://141.8.194.196:4000/',
+  //websiteAddress: 'http://localhost:4000/',
 }
--- layout/js/config.js
+++ layout/js/config.js
@@ -1,6 +1,6 @@
 const Config = {
-  //websiteAddress: 'http://141.8.194.196:4000/',
-  websiteAddress: 'http://localhost:4000/',
+  websiteAddress: 'http://141.8.194.196:4000/',
+  //websiteAddress: 'http://localhost:4000/',
 }
 
 export default Config;
