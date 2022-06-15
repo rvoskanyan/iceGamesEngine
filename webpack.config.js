883e9ffc14628e30c42275d834b725d923d780bb
rvoskanyan <rvoskanyan@mmtr.ru>
2022-06-15 15:25:59 +0300

    parsing

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ export default {
     path: path.resolve(__dirname, 'layout'),
   },
   optimization: {
-    minimize: false,
+    minimize: true,
     minimizer: [
       new CssMinimizerWebpackPlugin(),
       new TerserWebpackPlugin()
