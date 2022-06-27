42bef7f81bec80d1c8390808f635a8f5d28a7461
rvoskanyan <rvoskanyan@mmtr.ru>
2022-06-27 18:51:28 +0300

    normalize name product

--- services/parsing.js
+++ services/parsing.js
@@ -56,7 +56,7 @@ export const startParsingProducts = async () => {
           productOnSite.inStock = inStock;
           productOnSite.priceTo = priceTo;
           productOnSite.name = productOnSite.name.split(' ').map(item => {
-            if (item === '-' || item === '--') {
+            if (item === '—' || item === '--') {
               return '-';
             }
   
