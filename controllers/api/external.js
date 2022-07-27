85e4a11eaae9e28a5969ffb1e9b0ea14f4237ce3
rvoskanyan <rvoskanyan@mmtr.ru>
2022-07-27 15:27:02 +0300

    meta desc

--- controllers/api/external.js
+++ controllers/api/external.js
@@ -102,10 +102,10 @@ export const getFeedCsv = async (req, res) => {
     const workbook = new exceljs.Workbook();
     const worksheet = workbook.addWorksheet('Products In Stock');
   
-    worksheet.columns = [{header: 'ID,Title,URL,Image,Description,Price,Old Price,Currency,', key: 'row'}];
+    worksheet.columns = [{header: 'ID,ID2,Title,URL,Image,Description,Price,Old Price,Currency,', key: 'row'}];
   
     products.forEach((product, index) => {
-      worksheet.addRow({row: `${index},${product.name},${res.locals.websiteAddress}games/${product.alias},${res.locals.websiteAddress}${product.img},Купить игру ${product.name} c активацией в ${product.activationServiceId.name} со скидкой.,${product.priceTo},${product.priceFrom},RUB`});
+      worksheet.addRow({row: `${index},,${product.name},${res.locals.websiteAddress}games/${product.alias},${res.locals.websiteAddress}${product.img},Купить игру ${product.name} c активацией в ${product.activationServiceId.name} со скидкой.,${product.priceTo},${product.priceFrom},RUB`});
     });
   
     await workbook.csv.writeFile(path.join(__dirname, 'uploadedFiles/feed.csv'));
