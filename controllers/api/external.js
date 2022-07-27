3219800b380b0950ee5520451ce9d819e7537514
rvoskanyan <rvoskanyan@mmtr.ru>
2022-07-27 15:16:42 +0300

    meta desc

--- controllers/api/external.js
+++ controllers/api/external.js
@@ -105,7 +105,7 @@ export const getFeedCsv = async (req, res) => {
     worksheet.columns = [{header: 'ID,URL,Image,Title,Description,Price,Currency,Old Price', key: 'row', width: 10}];
   
     products.forEach((product, index) => {
-      worksheet.addRow({row: `${index},${res.locals.websiteAddress}games/${product.alias},${res.locals.websiteAddress}${product.img},${product.name},Купить игру ${product.name} c активацией в ${product.activationServiceId.name} со скидкой.,${product.priceTo},RUB,${product.priceFrom}`});
+      worksheet.addRow({row: `${index},${res.locals.websiteAddress}games/${product.alias},${res.locals.websiteAddress}${product.img},${product.name},Купить игру ${product.name} c активацией в ${product.activationServiceId.name} со скидкой.,${product.priceTo},RUB`});
     });
   
     await workbook.csv.writeFile(path.join(__dirname, 'uploadedFiles/feed.csv'));
