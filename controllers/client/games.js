0286f5421116e8570aa5d56cea0acdd30b1c5b94
rvoskanyan <rvoskanyan@mmtr.ru>
2023-01-26 14:57:00 +0300

    change title for catalog on activation services

--- controllers/client/games.js
+++ controllers/client/games.js
@@ -113,7 +113,7 @@ export const gamesPage = async (req, res, next) => {
         case 'activationServices': {
           metaDescription = `Ключи для ${section.name} в магазине ICE GAMES. Мгновенная доставка ключей активации. Широкий выбор игр, сервисная поддержка. Страница ${page}`;
           title = `Ключи для каталога игр ${section.name} со скидкой в магазине ICE GAMES — страница ${page}`;
-          hTitle = `Купить ключи активации ${section.name} дешево`;
+          hTitle = `Купить ключи активации ${section.name}`;
           activationServices.push(sectionName);
           break;
         }
