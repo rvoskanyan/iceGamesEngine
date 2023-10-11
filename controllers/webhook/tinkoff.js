6ca7596821dfc4cabe07ced9dda6f7c89d0eca5b
rvoskanyan <rvoskanyan@mmtr.ru>
2023-10-11 21:08:55 +0300

    fix

--- controllers/webhook/tinkoff.js
+++ controllers/webhook/tinkoff.js
@@ -8,6 +8,7 @@ import Product from "../../models/Product.js";
 
 export default async function (req, res) {
     try {
+        console.log(req.body);
         const {OrderId, Success, Status, Amount} = req.body;
         const order = await Order.findById(OrderId);
     
@@ -16,8 +17,6 @@ export default async function (req, res) {
         if (!order) {
             return res.status(404).json({err:true, messages:"Forbidden"}); //Сделать логирование и уведомление, что пришло уведомление по не существующему заказу
         }
-    
-        console.log(req.body);
 
         if (Success && Status === 'CONFIRMED') {
             if (order.status === 'paid') {
