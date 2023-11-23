e5764db5b9d8708f367c4d37f7e1fd64649a61c1
rvoskanyan <rvoskanyan@mmtr.ru>
2023-11-23 11:59:58 +0300

    fix

--- controllers/client/fillUpSteam.js
+++ controllers/client/fillUpSteam.js
@@ -72,24 +72,25 @@ export const checkStatus = async (req, res) => {
     }
     
     if (fillUp.status === 'pending') {
-      try {
-        const response = await fetch(`https://steam.kupikod.com/api/v3/partner-order/${fillUp.orderId}`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'token': 'icegame.store_q4L4Re1u1hIjQIgPBWqiDYZfzheIRmHEwAzX',
-          },
-        });
+      const response = await fetch(`https://steam.kupikod.com/api/v3/partner-order/${fillUp.orderId}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'token': 'icegame.store_q4L4Re1u1hIjQIgPBWqiDYZfzheIRmHEwAzX',
+        },
+      });
   
-        const stateData = await response.json();
+      const stateData = await response.json();
   
-        if (stateData.state !== 'pending') {
+      if (stateData.state !== 'pending') {
+        try {
           fillUp.codeOrderError = stateData.status;
           fillUp.status = stateData.state;
           await fillUp.save();
+        } catch (e) {
+          console.log(e);
+          fillUp.status = 'pending'
         }
-      } catch (e) {
-        console.log(e);
       }
     }
     
