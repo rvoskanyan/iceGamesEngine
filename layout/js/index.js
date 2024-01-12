e37b0317803f238f1dea9d868f971517c56eee1c
Tikhon Izmailov <tikhonbright@gmail.com>
2024-01-12 15:16:17 +0700

    ICESUP-63 home page redesign

--- layout/js/index.js
+++ layout/js/index.js
@@ -5,12 +5,10 @@ import PopupController from "./PopupController.js";
 import Postman from "./Postman.js";
 import Prompt from "./Prompt.js";
 import Slider from "./Slider.js";
-import SliderHome from "./SliderHome.js";
+import { popularsSwiper, swiperSelections, swiperGenres, swiperNews } from "./SliderHome.js";
 import Tabs from "./Tabs.js";
 import { websiteAddress } from "./config.js";
 import { debounce, getProductCardNode, scrollTo, urlEncodeFormData } from "./utils.js";
-
-
 import './../styles/index.sass';
 import AsyncFormSteam from './AsyncFormSteam.js';
 import Message from "./lib/message.js";
@@ -37,8 +35,6 @@ const achievementViewProfileSliderNode = document.querySelector('.js-achievement
 const homeMediaSliderNode = document.querySelector('.js-homeMediaSlider');
 const productsFromArticleSliderNode = document.querySelector('.js-productsFromArticleSlider');
 const homeCatalogTabsNode = document.querySelector('.js-homeCatalogTabs');
-const genresSliderNode = document.querySelector('.js-genresSlider');
-const selectionsSliderNode = document.querySelector('.js-selectionsSlider');
 const gameGallerySliderNode = document.querySelector('.js-gameGallerySlider');
 const gameInfoTabsNode = document.querySelector('.js-gameInfoTabs');
 const gameInfoSystemParamsNode = document.querySelector('.js-gameInfoSystemParams');
@@ -1767,6 +1763,10 @@ if (gamePageNode) {
 }
 
 if (homeSliderNode) {
+    popularsSwiper()
+    swiperSelections()
+    swiperGenres()
+    swiperNews()
     const addToCartBtn = homeSliderNode.querySelectorAll('.js-addToCart');
     let playVideoTimeOutId;
 
@@ -1868,22 +1868,6 @@ if (homeCatalogTabsNode) {
         mainNode: homeCatalogTabsNode,
     });
 }
-if (genresSliderNode) {
-    new Slider({
-        mainNode: genresSliderNode,
-        isTrack: true,
-        countSlidesScroll: 1,
-        switchingTime: 5000,
-        infinity: true,
-    });
-}
-
-// ########################################################
-
-if (selectionsSliderNode) {
-}
-
-// ########################################################
 
 
 if (gameGallerySliderNode) {
